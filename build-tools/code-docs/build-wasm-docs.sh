4a50f35b2e4cc7c7175c32ab27041e5b2148d065
waytothevenus <andyhung772@gmail.com>
2024-12-06 15:01:54 -0800

    fix(backend): migrate to use backend crates

--- build-tools/code-docs/build-wasm-docs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the dir where the script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-cd ${SCRIPT_DIR}/../../
-cargo run -p wasm-doc-gen -- -o wasm-wrappers/WASM-API.md

61c7ee6397ab30e5f8b7210001d31f1094ce9293
waytothevenus <andyhung772@gmail.com>
2024-12-20 14:00:07 -0400

    fix(frontend): fix issue in chainInfo event listener

--- node-gui-x/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-const flowbite = require("flowbite-react/tailwind");
-module.exports = {
-  content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}", flowbite.content()],
-  theme: {
-    extend: {},
-  },
-  plugins: [flowbite.plugin()],
-};

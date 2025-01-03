e09df3f77e3b2d82e347d1ecce298edc01db774f
waytothevenus <andyhung772@gmail.com>
2025-01-03 12:04:45 -0400

    fix(frontend): fix issue in closing wallet

--- src/components/WalletActions.tsx
+++ src/components/WalletActions.tsx
@@ -144,214 +144,224 @@ const WalletActions = (props: {
   };
 
   return (
-    <div className="bg-white border border-gray-200 shadowoverflow-y-auto mt-8 p-8 m-8 rounded-lg shadow">
-      {showEncryptWalletModal && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="bg-white rounded-lg shadow-lg z-10 p-4 max-w-lg mx-auto relative space-y-4">
-            <button
-              className="absolute top-2 right-2 bg-transparent border-none shadow-none focus:outline-none "
-              onClick={() => setShowEncryptWalletModal(false)}
-            >
-              <IoCloseSharp />
-            </button>
-            <h2 className="text-lg font-bold mb-4">Encrypt Wallet</h2>
-            <input
-              placeholder="Enter password"
-              type="password"
-              className="w-full rounded-lg"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <input
-              placeholder="Repeat password"
-              type="password"
-              className="w-full rounded-lg"
-              value={confirmPassword}
-              onChange={(e) => handleConfirmPasswordChange(e.target.value)}
-            />
-            {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
-            <button
-              className="bg-green-400 text-black w-full px-2 py-1 rounded-lg hover:bg-[#000000] hover:text-green-400 transition duration-200"
-              onClick={() =>
-                handleWalletAction("set_password", { password: password })
-              }
-            >
-              Encrypt Wallet
-            </button>
-          </div>
-        </div>
-      )}
-      {showUnlockModal && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="bg-white rounded-lg shadow-lg z-10 p-4 max-w-lg mx-auto relative space-y-4">
-            {/* Close Button */}
-            <button
-              className="absolute top-2 right-2 bg-transparent border-none shadow-none focus:outline-none "
-              onClick={() => setShowUnlockModal(false)}
-            >
-              <IoCloseSharp />
-            </button>
-            <h2 className="text-lg font-bold mb-4">Unlock</h2>
-            <input
-              placeholder="Enter a password"
-              type="password"
-              className="w-full rounded-lg"
-              value={unLockPassword}
-              onChange={(e) => setUnLockPassword(e.target.value)}
-            />
-
-            <button
-              className="bg-green-400 text-black w-full px-2 py-1 rounded-lg hover:bg-[#000000] hover:text-green-400 transition duration-200"
-              onClick={() =>
-                handleWalletAction("unlock", { password: unLockPassword })
-              }
-            >
-              Unlock
-            </button>
-          </div>
-        </div>
-      )}
-      <div className="row flex items-center justify-between pl-8 pr-8 pb-0">
-        <div>
-          <span className="flex space-x-2">
-            <div className="font-thin">My balance: </div>
-            <div className="font-bold">
-              {props.currentAccount?.balance?.coins?.decimal}{" "}
-              {props.netMode === "Mainnet" ? "ML" : "TML"}
+    <>
+      {props.currentWallet && (
+        <div className="bg-white border border-gray-200 overflow-y-auto mt-8 p-8 m-8 rounded-lg shadow">
+          {showEncryptWalletModal && (
+            <div className="fixed inset-0 flex items-center justify-center z-50">
+              <div className="absolute inset-0 bg-black opacity-50"></div>
+              <div className="bg-white rounded-lg shadow-lg z-10 p-4 max-w-lg mx-auto relative space-y-4">
+                <button
+                  className="absolute top-2 right-2 bg-transparent border-none shadow-none focus:outline-none "
+                  onClick={() => setShowEncryptWalletModal(false)}
+                >
+                  <IoCloseSharp />
+                </button>
+                <h2 className="text-lg font-bold mb-4">Encrypt Wallet</h2>
+                <input
+                  placeholder="Enter password"
+                  type="password"
+                  className="w-full rounded-lg"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                  placeholder="Repeat password"
+                  type="password"
+                  className="w-full rounded-lg"
+                  value={confirmPassword}
+                  onChange={(e) => handleConfirmPasswordChange(e.target.value)}
+                />
+                {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+                <button
+                  className="bg-green-400 text-black w-full px-2 py-1 rounded-lg hover:bg-[#000000] hover:text-green-400 transition duration-200"
+                  onClick={() =>
+                    handleWalletAction("set_password", { password: password })
+                  }
+                >
+                  Encrypt Wallet
+                </button>
+              </div>
             </div>
-          </span>
-        </div>
-        <div className="space-x-2">
-          {walletState === "EnabledLocked" && (
-            <button
-              className="py-1 px-2 rounded-lg bg-[#69EE96] text-[#000000] hover:text-[#69EE96] hover:bg-black "
-              onClick={() => {
-                setShowUnlockModal(true);
-              }}
-            >
-              UnLock
-            </button>
           )}
-          {walletState === "EnabledUnlocked" && (
-            <button
-              className="py-1 px-2 rounded-lg bg-[#69EE96] text-[#000000] hover:text-[#69EE96] hover:bg-black "
-              onClick={() => handleWalletAction("lock", {})}
-            >
-              Lock
-            </button>
+          {showUnlockModal && (
+            <div className="fixed inset-0 flex items-center justify-center z-50">
+              <div className="absolute inset-0 bg-black opacity-50"></div>
+              <div className="bg-white rounded-lg shadow-lg z-10 p-4 max-w-lg mx-auto relative space-y-4">
+                {/* Close Button */}
+                <button
+                  className="absolute top-2 right-2 bg-transparent border-none shadow-none focus:outline-none "
+                  onClick={() => setShowUnlockModal(false)}
+                >
+                  <IoCloseSharp />
+                </button>
+                <h2 className="text-lg font-bold mb-4">Unlock</h2>
+                <input
+                  placeholder="Enter a password"
+                  type="password"
+                  className="w-full rounded-lg"
+                  value={unLockPassword}
+                  onChange={(e) => setUnLockPassword(e.target.value)}
+                />
+
+                <button
+                  className="bg-green-400 text-black w-full px-2 py-1 rounded-lg hover:bg-[#000000] hover:text-green-400 transition duration-200"
+                  onClick={() =>
+                    handleWalletAction("unlock", { password: unLockPassword })
+                  }
+                >
+                  Unlock
+                </button>
+              </div>
+            </div>
           )}
-          {walletState !== "EnabledLocked" && (
-            <button
-              className={`py-1 px-2 ${
-                !props.currentWallet
-                  ? "bg-gray-400 cursor-not-allowed"
-                  : "bg-[#69EE96] hover:text-[#69EE96] hover:bg-black"
-              } rounded-lg bg-[#69EE96] text-[#000000] hover:text-[#69EE96] hover:bg-black`}
-              onClick={() => {
-                walletState === "EnabledUnlocked"
-                  ? handleWalletAction("remove_password", {})
-                  : setShowEncryptWalletModal(true);
-              }}
-            >
-              {walletState === "EnabledUnlocked"
-                ? "Disable Wallet Encryption"
-                : "Encrypt Wallet"}
-            </button>
+          <div className="row flex items-center justify-between pl-8 pr-8 pb-0">
+            <div>
+              <span className="flex space-x-2">
+                <div className="font-thin">My balance: </div>
+                <div className="font-bold">
+                  {props.currentAccount?.balance?.coins?.decimal}{" "}
+                  {props.netMode === "Mainnet" ? "ML" : "TML"}
+                </div>
+              </span>
+            </div>
+            <div className="space-x-2">
+              {walletState === "EnabledLocked" && (
+                <button
+                  className="py-1 px-2 rounded-lg bg-[#69EE96] text-[#000000] hover:text-[#69EE96] hover:bg-black "
+                  onClick={() => {
+                    setShowUnlockModal(true);
+                  }}
+                >
+                  UnLock
+                </button>
+              )}
+              {walletState === "EnabledUnlocked" && (
+                <button
+                  className="py-1 px-2 rounded-lg bg-[#69EE96] text-[#000000] hover:text-[#69EE96] hover:bg-black "
+                  onClick={() => handleWalletAction("lock", {})}
+                >
+                  Lock
+                </button>
+              )}
+              {walletState !== "EnabledLocked" && (
+                <button
+                  className={`py-1 px-2 ${
+                    !props.currentWallet
+                      ? "bg-gray-400 cursor-not-allowed"
+                      : "bg-[#69EE96] hover:text-[#69EE96] hover:bg-black"
+                  } rounded-lg bg-[#69EE96] text-[#000000] hover:text-[#69EE96] hover:bg-black`}
+                  onClick={() => {
+                    walletState === "EnabledUnlocked"
+                      ? handleWalletAction("remove_password", {})
+                      : setShowEncryptWalletModal(true);
+                  }}
+                >
+                  {walletState === "EnabledUnlocked"
+                    ? "Disable Wallet Encryption"
+                    : "Encrypt Wallet"}
+                </button>
+              )}
+              <button
+                className={`py-1 px-4 ${
+                  !props.currentWallet
+                    ? "bg-gray-400 cursor-not-allowed"
+                    : "bg-white hover:outline-none hover:bg-[#E02424] hover:text-white hover:border-[#E02424]"
+                } mt-8 mb-8 border text-[#E02424] border-[#E02424]  rounded-lg transition-all duration-200 `}
+                onClick={() =>
+                  handleCloseWallet(
+                    props.currentWallet?.wallet_id
+                      ? props.currentWallet.wallet_id
+                      : 0
+                  )
+                }
+              >
+                Close Wallet
+              </button>
+            </div>
+          </div>
+          {props.activeTab === "transactions" && (
+            <Transactions
+              transactions={props.currentAccount?.transaction_list}
+            />
           )}
-          <button
-            className={`py-1 px-4 ${
-              !props.currentWallet
-                ? "bg-gray-400 cursor-not-allowed"
-                : "bg-white hover:outline-none hover:bg-[#E02424] hover:text-white hover:border-[#E02424]"
-            } mt-8 mb-8 border text-[#E02424] border-[#E02424]  rounded-lg transition-all duration-200 `}
-            onClick={() =>
-              handleCloseWallet(
+          {props.activeTab === "addresses" && (
+            <Addresses
+              isLoading={props.isLoading}
+              setIsLoading={props.setIsLoading}
+              loadingMessage={props.loadingMessage}
+              setLoadingMessage={props.setLoadingMessage}
+              addresses={
+                props.currentAccount?.addresses
+                  ? props.currentAccount.addresses
+                  : {}
+              }
+              walletId={
                 props.currentWallet?.wallet_id
                   ? props.currentWallet.wallet_id
                   : 0
-              )
-            }
-          >
-            Close Wallet
-          </button>
+              }
+              accountId={props.currentAccountId}
+              handleUpdateCurrentAccount={props.handleUpdateCurrentAccount}
+            />
+          )}
+          {props.activeTab === "send" && (
+            <Send
+              isLoading={props.isLoading}
+              setIsLoading={props.setIsLoading}
+              loadingMessage={props.loadingMessage}
+              setLoadingMessage={props.setLoadingMessage}
+              currentAccount={props.currentAccount}
+              walletId={
+                props.currentWallet?.wallet_id
+                  ? props.currentWallet.wallet_id
+                  : 0
+              }
+              accountId={props.currentAccountId}
+            />
+          )}
+          {props.activeTab === "staking" && (
+            <Staking
+              isLoading={props.isLoading}
+              setIsLoading={props.setIsLoading}
+              loadingMessage={props.loadingMessage}
+              setLoadingMessage={props.setLoadingMessage}
+              chainInfo={props.chainInfo?.chain_info}
+              currentAccount={props.currentAccount}
+              currentWallet={props.currentWallet}
+              stakingBalances={props.stakingBalances}
+              currentAccountId={props.currentAccountId}
+              currentWalletId={props.currentWallet?.wallet_id}
+              handleUpdateStakingState={props.handleUpdateStakingState}
+            />
+          )}
+          {props.activeTab === "delegation" && (
+            <Delegation
+              isLoading={props.isLoading}
+              setIsLoading={props.setIsLoading}
+              loadingMessage={props.loadingMessage}
+              setLoadingMessage={props.setLoadingMessage}
+              currentAccount={props.currentAccount}
+              currentAccountId={props.currentAccountId}
+              delegationBalances={props.delegationBalances}
+              currentWallet={props.currentWallet}
+              empty_consensus_reward_maturity_block_count={
+                props.chainInfo?.empty_consensus_reward_maturity_block_count
+                  ? props.chainInfo?.empty_consensus_reward_maturity_block_count
+                  : 0
+              }
+            />
+          )}
+          {props.activeTab === "console" && (
+            <Console
+              currentAccount={props.currentAccount}
+              currentWallet={props.currentWallet}
+              currentAccountId={props.currentAccountId}
+            />
+          )}
         </div>
-      </div>
-      {props.activeTab === "transactions" && (
-        <Transactions transactions={props.currentAccount?.transaction_list} />
       )}
-      {props.activeTab === "addresses" && (
-        <Addresses
-          isLoading={props.isLoading}
-          setIsLoading={props.setIsLoading}
-          loadingMessage={props.loadingMessage}
-          setLoadingMessage={props.setLoadingMessage}
-          addresses={
-            props.currentAccount?.addresses
-              ? props.currentAccount.addresses
-              : {}
-          }
-          walletId={
-            props.currentWallet?.wallet_id ? props.currentWallet.wallet_id : 0
-          }
-          accountId={props.currentAccountId}
-          handleUpdateCurrentAccount={props.handleUpdateCurrentAccount}
-        />
-      )}
-      {props.activeTab === "send" && (
-        <Send
-          isLoading={props.isLoading}
-          setIsLoading={props.setIsLoading}
-          loadingMessage={props.loadingMessage}
-          setLoadingMessage={props.setLoadingMessage}
-          currentAccount={props.currentAccount}
-          walletId={
-            props.currentWallet?.wallet_id ? props.currentWallet.wallet_id : 0
-          }
-          accountId={props.currentAccountId}
-        />
-      )}
-      {props.activeTab === "staking" && (
-        <Staking
-          isLoading={props.isLoading}
-          setIsLoading={props.setIsLoading}
-          loadingMessage={props.loadingMessage}
-          setLoadingMessage={props.setLoadingMessage}
-          chainInfo={props.chainInfo?.chain_info}
-          currentAccount={props.currentAccount}
-          currentWallet={props.currentWallet}
-          stakingBalances={props.stakingBalances}
-          currentAccountId={props.currentAccountId}
-          currentWalletId={props.currentWallet?.wallet_id}
-          handleUpdateStakingState={props.handleUpdateStakingState}
-        />
-      )}
-      {props.activeTab === "delegation" && (
-        <Delegation
-          isLoading={props.isLoading}
-          setIsLoading={props.setIsLoading}
-          loadingMessage={props.loadingMessage}
-          setLoadingMessage={props.setLoadingMessage}
-          currentAccount={props.currentAccount}
-          currentAccountId={props.currentAccountId}
-          delegationBalances={props.delegationBalances}
-          currentWallet={props.currentWallet}
-          empty_consensus_reward_maturity_block_count={
-            props.chainInfo?.empty_consensus_reward_maturity_block_count
-              ? props.chainInfo?.empty_consensus_reward_maturity_block_count
-              : 0
-          }
-        />
-      )}
-      {props.activeTab === "console" && (
-        <Console
-          currentAccount={props.currentAccount}
-          currentWallet={props.currentWallet}
-          currentAccountId={props.currentAccountId}
-        />
-      )}
-    </div>
+    </>
   );
 };
 export default WalletActions;

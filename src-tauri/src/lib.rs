4e8d7c40f78068a9a0d423b504d8a1622e05d8e9
waytothevenus <andyhung772@gmail.com>
2025-01-17 04:30:18 -0400

    fix(tauri): modify license version

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// https://github.com/mintlayer/mintlayer-core/blob/master/LICENSE
+// https://github.com/mintlayer/node-gui-x/blob/master/LICENSE
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
